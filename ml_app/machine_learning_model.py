0240c626fecc0338dd0fd5db68004778e05fafc2
Kempf <Prd7fvsq>
2020-11-09 23:40:10 +0100

    Death on Titanic ml predict

--- ml_app/machine_learning_model.py
+++ ml_app/machine_learning_model.py
@@ -1,2 +1,24 @@
+
+
 def multiplier(num):
     return int(num) * 100
+
+
+def fake_prediction(age):
+    if int(age) > 10:
+        return "Survive"
+    else:
+        return "Super survive"
+
+
+def prediction_model(pclass, sex, age, sibsp, parch, fare, embarked, title):
+    import pickle
+    x = [[pclass, sex, age, sibsp, parch, fare, embarked, title]]
+    randomforest = pickle.load(open('ml_app/titanic_model.sav', 'rb'))
+    prediction = randomforest.predict(x)
+    if prediction == 1:
+        return 'Survived'
+    elif prediction == 0:
+        return 'Dead'
+    else:
+        return 'Error'
--- ml_app/views.py
+++ ml_app/views.py
@@ -8,8 +8,15 @@ def home(request):
 def contact(request):
     return render(request, 'contact.html')
 
-
+# pclass, sex, age, sibsp, parch, fare, embarked, title
 def result(request):
-    user_input = request.GET['user_input']
-    user_input = ml.multiplier(user_input)
-    return render(request, 'result.html', {'home_input': user_input})
+    pclass = int(request.GET['pclass'])
+    sex = int(request.GET['sex'])
+    age = int(request.GET['age'])
+    sibsp = int(request.GET['sibsp'])
+    parch = int(request.GET['parch'])
+    fare = int(request.GET['fare'])
+    embarked = int(request.GET['embarked'])
+    title = int(request.GET['title'])
+    prediction_result = ml.prediction_model(pclass, sex, age, sibsp, parch, fare, embarked, title)
+    return render(request, 'result.html', {'prediction': prediction_result})
